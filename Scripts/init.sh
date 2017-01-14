fa931c04dd0f6dd3c053b95e78284bc067e55c78
architsharma97 <archit97sharma@gmail.com>
2017-01-14 22:57:56 +0530

    Started file for Late Fusion Encoder

--- Scripts/init.sh
+++
@@ -1,5 +0,0 @@
-mkdir Data
-cd Data/
-wget http://computing.ece.vt.edu/~abhshkdz/data/visdial/visdial_0.5_train.json
-wget http://computing.ece.vt.edu/~abhshkdz/data/visdial/visdial_0.5_val.json
-wget http://computing.ece.vt.edu/~abhshkdz/data/visdial/visdial_0.5_test.json
