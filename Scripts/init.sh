7c93fbffa92ac7c69be0f99955ecb0565cc0b715
architsharma97 <archit97sharma@gmail.com>
2017-01-14 15:08:50 +0530

    Initial commit

---
+++ Scripts/init.sh
@@ -0,0 +1,5 @@
+mkdir Data
+cd Data/
+wget http://computing.ece.vt.edu/~abhshkdz/data/visdial/visdial_0.5_train.json
+wget http://computing.ece.vt.edu/~abhshkdz/data/visdial/visdial_0.5_val.json
+wget http://computing.ece.vt.edu/~abhshkdz/data/visdial/visdial_0.5_test.json
