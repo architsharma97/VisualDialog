e664ab02db8daa295e813e408d7960a5137fc7ab
architsharma97 <archit97sharma@gmail.com>
2017-01-19 14:49:36 +0530

    Added script to download the glove embeddings

--- Data/Embeddings/download.sh
+++ Data/Embeddings/download.sh
@@ -1,9 +1,16 @@
-# uncomment files to download
+# uncomment files to download: defaults to glove embeddings
 # pre-trained glove embeddings with 400k words
-# wget http://nlp.stanford.edu/data/glove.6B.zip
+# defaults to using the 300 dimensional embeddings from {50,100,200,300}
+# change appropriately
+# uncomment to delete the rest of the embeddings
+wget http://nlp.stanford.edu/data/glove.6B.zip
+unzip glove.6B.zip .
+mv /glove.6B/glove.6B.300d.txt .
+# rm -rf glove.6B/
+rm -rf glove.6B.zip
 
 # pre-trained glove embeddings with 1.9M words and 300d embeddings
-wget http://nlp.stanford.edu/data/glove.42B.300d.zip
+# wget http://nlp.stanford.edu/data/glove.42B.300d.zip
 
 # pre-trained word2vec embeddings for 3M words and 300d embeddings
 # follow the link: https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit?usp=sharing
