b427bebd6f5fea308c1ee17ef7e7ea7c8940be3d
architsharma97 <archit97sharma@gmail.com>
2017-01-23 23:34:06 +0530

    Fixed bugs in embedding matrix generator

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -11,22 +11,24 @@ def vgg16():
 	Returns the VGG16 model which outputs the 4096 dimensional embeddings
 	of images. To be used with process_image.
 	'''
-    print "Building VGG16 model"
-    base_model=VGG16(weights='imagenet',include_top=True)
-    model=Model(input=base_model.input, output=base_model.get_layer('fc2').output)
-    return model
+	print "Building VGG16 model"
+	base_model=VGG16(weights='imagenet',include_top=True)
+	model=Model(input=base_model.input, output=base_model.get_layer('fc2').output)
+	return model
 
 def process_image(img_path):
 	'''
 	img_path: path to the actual image
 	'''
 	print "Pre-processing image"
-    img = image.load_img(img_path, target_size=(224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    return x
+	img = image.load_img(img_path, target_size=(224, 224))
+	x = image.img_to_array(img)
+	x = np.expand_dims(x, axis=0)
+	x = preprocess_input(x)
+	return x
 
+# typical path for glove embeddings if setup using default scripts
+# '../../Data/Embeddings/glove.6B.300d.txt'
 def get_embeddings(vocab, path, glove=True, EMBEDDING_SIZE=300):
 	'''
 	vocab: Given a dictionary of words, returns an embedding matrix.
@@ -37,8 +39,8 @@ def get_embeddings(vocab, path, glove=True, EMBEDDING_SIZE=300):
 	Defaults to 300.
 	'''
 
-	print "Constructing the embedding matrix of Vocabulary"
-	embeddings=np.zeros(len(vocab),EMBEDDING_SIZE)
+	print "Constructing Embedding Matrix for given Vocabulary"
+	embeddings=np.zeros((len(vocab),EMBEDDING_SIZE))
 
 	# do not read in the whole embeddings file
 	if glove:
@@ -46,7 +48,9 @@ def get_embeddings(vocab, path, glove=True, EMBEDDING_SIZE=300):
 			for line in embeddings_file:
 				entry=line.split()
 				if entry[0] in vocab:
-					embeddings[vocab[entry[0]]]=np.asarray(values[1:], dtype='float32')
+					embeddings[vocab[entry[0]]]=np.asarray(entry[1:], dtype='float32')
+	else:
+		print "Cannot construct Embedding matrix from pre-trained Word2Vec"
 
 	print "Constructed Embedding Matrix"
 	return embeddings
