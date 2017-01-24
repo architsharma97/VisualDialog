360ae9dd948c605bce1315a7405417077bdeb57f
architsharma97 <archit97sharma@gmail.com>
2017-01-24 11:05:53 +0530

    Added embeddings for word2vec

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -4,6 +4,7 @@ from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 from keras.models import Model
 import numpy as np
+from gensim.models import Word2Vec
 
 # use model.predict(process_image(img_path)) to get the fc2 layer output
 def vgg16():
@@ -34,23 +35,28 @@ def get_embeddings(vocab, path, glove=True, EMBEDDING_SIZE=300):
 	vocab: Given a dictionary of words, returns an embedding matrix.
 		   Each word should be mapped to a unique index < len(vocab).
 	glove: Defaults to text file format of glove embeddings.
+		   Turn false for Word2Vec.
 	path: Provide a path to the required embeddings.
 	EMBEDDING_SIZE: Size of the embeddings in the described file. 
 	Defaults to 300.
 	'''
 
-	print "Constructing Embedding Matrix for given Vocabulary"
 	embeddings=np.zeros((len(vocab),EMBEDDING_SIZE))
 
 	# do not read in the whole embeddings file
 	if glove:
+		print "Constructing Embedding Matrix for given Vocabulary using GloVe pre-trained embeddings"
 		with open(path) as embeddings_file:
 			for line in embeddings_file:
 				entry=line.split()
 				if entry[0] in vocab:
 					embeddings[vocab[entry[0]]]=np.asarray(entry[1:], dtype='float32')
 	else:
-		print "Cannot construct Embedding matrix from pre-trained Word2Vec"
+		print "Constructing Embedding Matrix for given Vocabulary using Word2Vec embeddings"
+		model=Word2Vec.load_word2vec_format(path,binary=True)
+		for key, idx in vocab.iteritems():
+			embeddings[idx]=model[key]
+		# print "Cannot construct Embedding matrix from pre-trained Word2Vec"
 
 	print "Constructed Embedding Matrix"
 	return embeddings
