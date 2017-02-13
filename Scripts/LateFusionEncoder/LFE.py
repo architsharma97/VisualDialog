d854521459a5b0b9362a4b5b6fc44a6689db9b3b
architsharma97 <archit97sharma@gmail.com>
2017-02-14 02:33:33 +0530

    Completed decoder for Late fusion encoder

--- Scripts/LateFusionEncoder/LFE.py
+++ Scripts/LateFusionEncoder/LFE.py
@@ -12,51 +12,68 @@ from basic_layers import param_init_fflayer, param_init_lstm, fflayer, lstm_laye
 from collections import OrderedDict
 
 # constants
-DATA_DIR='../../Data/'
+DATA_DIR = '../../Data/'
 
 # VGG16 Specification
-IMAGE_DIM=4096
+IMAGE_DIM = 4096
 
 # LSTMs Specifications: H=>QA-History+Caption Encoder, Q=>Question Encoder 
-LSTM_H_OUT=512
-LSTM_H_LAYERS=2
-lstm_prefix_h='lstm_h'
+LSTM_H_OUT = 512
+LSTM_H_LAYERS = 2
+lstm_prefix_h = 'lstm_h'
 
-LSTM_Q_OUT=512
-LSTM_Q_LAYERS=2
-lstm_prefix_q='lstm_q'
+LSTM_Q_OUT = 512
+LSTM_Q_LAYERS = 2
+lstm_prefix_q = 'lstm_q'
 
-# Fully Connected Layer Specification
-FF_IN=IMAGE_DIM+LSTM_Q_OUT+LSTM_H_OUT
-FF_OUT=512
-ff_prefix='ff'
+# fully Connected Layer Specification
+FF_IN = IMAGE_DIM + LSTM_Q_OUT + LSTM_H_OUT
+FF_OUT = 512
+ff_prefix = 'ff'
 
-def initialize_lfe():
+# decoder layers
+LSTM_D = FF_OUT
+LSTM_D_LAYERS = 2
+lstm_prefix_d = 'lstm_d'
+
+# load the embeddings
+embeddings = T.shared(np.transpose(np.load(DATA_DIR + 'embedding_matrix.npy').astype('float32')))
+EMBEDDINGS_DIM = embeddings.shape[0]
+
+# loading dictionaries
+word_idx_map = load_obj(DATA_DIR + 'dictionary.pkl')
+idx_word_map = load_obj(DATA_DIR + 'reverse_dictionary.pkl')
+
+def initialize():
 	'''
-	Initialize the parameters for the late fusion encoder
+	Initialize the parameters for the late fusion encoder and decoder
 	'''
 	# Parameters for the model
-	params=OrderedDict()
+	params = OrderedDict()
 	global (DATA_DIR, IMAGE_DIM, LSTM_H_OUT, LSTM_H_LAYERS, lstm_prefix_h, 
 		lstm_prefix_q, LSTM_Q_LAYERS, LSTM_Q_OUT, FF_IN, 
-		FF_OUT, ff_prefix)
+		LSTM_D, LSTM_D_LAYERS, lstm_prefix_d, EMBEDDINGS_DIM, FF_OUT, ff_prefix)
 	
 	# feedforward layer
-	params=param_init_fflayer(params, ff_prefix, FF_IN, FF_OUT)
+	params = param_init_fflayer(params, ff_prefix, FF_IN, FF_OUT)
 
 	# lstm layers
 	# question encoding layers
-	params=param_init_lstm(params, _concat(lstm_prefix_q, 1), EMBEDDINGS_DIM, LSTM_Q_OUT)
-	params=param_init_lstm(params, _concat(lstm_prefix_q, 2), LSTM_Q_OUT, LSTM_Q_OUT)
+	params = param_init_lstm(params, _concat(lstm_prefix_q, 1), EMBEDDINGS_DIM, LSTM_Q_OUT)
+	params = param_init_lstm(params, _concat(lstm_prefix_q, 2), LSTM_Q_OUT, LSTM_Q_OUT)
 
 	# history encoding layers
-	params=param_init_lstm(params, _concat(lstm_prefix_h, 1), EMBEDDINGS_DIM, LSTM_H_OUT)
-	params=param_init_lstm(params, _concat(lstm_prefix_h, 2), LSTM_H_OUT, LSTM_H_OUT)
+	params = param_init_lstm(params, _concat(lstm_prefix_h, 1), EMBEDDINGS_DIM, LSTM_H_OUT)
+	params = param_init_lstm(params, _concat(lstm_prefix_h, 2), LSTM_H_OUT, LSTM_H_OUT)
 
-	# Initialize Theano Shared Variables for params
-	tparams=OrderedDict()
+	# decoding layers
+	params = param_init_lstm(params, _concat(lstm_prefix_d, 1), EMBEDDINGS_DIM, LSTM_D)
+	params = param_init_lstm(params, _concat(lstm_prefix_d, 2), LSTM_D, EMBEDDINGS_DIM)
+
+	# initialize theano shared variables for params
+	tparams = OrderedDict()
 	for key, val in params.iteritems():
-		tparams[key]=T.shared(params[key], name=key)
+		tparams[key] = T.shared(params[key], name=key)
 
 	return tparams
 
@@ -64,9 +81,10 @@ def build_lfe(tparams):
 	'''
 	Builds the computational graph of the encoder
 	'''
+	
 	global (DATA_DIR, IMAGE_DIM, LSTM_H_OUT, LSTM_H_LAYERS, lstm_prefix_h, 
 		lstm_prefix_q, LSTM_Q_LAYERS, LSTM_Q_OUT, FF_IN, 
-		FF_OUT, ff_prefix)
+		LSTM_D, LSTM_D_LAYERS, lstm_prefix_d, EMBEDDINGS_DIM, FF_OUT, ff_prefix)
 
 	# vgg16 extracted features for images
 	# data preparation ensures that the number of images matches the number of questions
@@ -110,15 +128,39 @@ def build_lfe(tparams):
 
 	return img, que, his, lfcode
 
+def build_decoder(tparams, lfcode):
+	'''
+	Builds computational graph for generative decoder
+	'''
+	
+	global (DATA_DIR, IMAGE_DIM, LSTM_H_OUT, LSTM_H_LAYERS, lstm_prefix_h, 
+		lstm_prefix_q, LSTM_Q_LAYERS, LSTM_Q_OUT, FF_IN, embeddings,
+		LSTM_D, LSTM_D_LAYERS, lstm_prefix_d, EMBEDDINGS_DIM, FF_OUT, ff_prefix)
 
-# load the embeddings
-embeddings=np.load(DATA_DIR+'embedding_matrix.npy').astype('float32')
-EMBEDDINGS_DIM=embeddings.shape[1]
+	# timesteps x number of samples x dim
+	ans = T.dtensor3('ans', dtype='float32')
 
-# loading dictionaries
-word_idx_map=load_obj(DATA_DIR+'dictionary.pkl')
-idx_word_map=load_obj(DATA_DIR+'reverse_dictionary.pkl')
+	asteps = a.shape[0]
+	# initial state set to late fusion encoder's output
+	out_6 = lstm_layer(tparams, ans, _concat(lstm_prefix_d, 1), init_state=lfcode, n_steps=asteps)
+
+	# resturctuing the output from previous layer
+	in_7 = T.zeros((asteps, ans.shape[1], out_6[0][0].shape[1]), dtype='float32')
+	for i in range(len(out_6)):
+		in_7[i,:,:] = out_6[i][0]
+
+	# initial state set to zero, can be changed to late fusion encoder's o/p
+	out_7 = lstm_layer(tparams, in_7, _concat(lstm_prefix_d, 2), n_steps=asteps)
+	
+	# computes softmax and arranges the output in timesteps x samples x softmax over dictionary
+	out = T.zeros((asteps, out_7[0][0].shape[0], embeddings.shape[1]), dtype='float32')
+	for i in range(asteps):
+		out[i,:,:] = T.nnet.softmax(T.dot(out_7[i][0], embeddings))
+
+	return ans, out
 
 # preprocess the training data to get input matrices and tensors
-image_features, questions_tensor, answers_tensor=preprocess(DATA_DIR, load_dict=True, load_embedding_data=True, save_data=False)
-tparams=initialize()
+image_features, questions_tensor, answers_tensor = preprocess(DATA_DIR, load_dict=True, load_embedding_data=True, save_data=False)
+tparams = initialize()
+img, que, his, lfcode = build_lfe(tparams)
+ans, out = build_decoder(tparams, lfcode)
