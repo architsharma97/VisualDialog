396f4744211d23fb4a943446fc6cf341d4bfc43a
architsharma97 <archit97sharma@gmail.com>
2017-03-23 09:15:54 +0530

    Trying smaller batch size

--- Scripts/LateFusionEncoder/late_fusion.py
+++ Scripts/LateFusionEncoder/late_fusion.py
@@ -120,7 +120,7 @@ EMBEDDINGS_DIM = embed.shape[0]
 
 if len(sys.argv) <=1 or int(sys.argv[1]) == 0:
 	print "Preparing minibatches"
-	train_data = minibatch.data(image_features, questions_tensor, answers_tensor, answers_tokens_idx, len(idx_word_map), batch_size=64)
+	train_data = minibatch.data(image_features, questions_tensor, answers_tensor, answers_tokens_idx, len(idx_word_map), batch_size=32)
 
 def initialize(address=None):
 	'''
